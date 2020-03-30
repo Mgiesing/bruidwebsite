c3d5292697cf6043b54c377d132e5d73ccc99b37
Marco <marcogiesing02@gmail.com>
2020-03-30 16:55:11 +0200

    Added comments

--- php/logoutListener.php
+++ php/logoutListener.php
@@ -1,7 +1,7 @@
 <?php
 //Start user data storage (so we know who is logged in)
 //Put the session to null so it basically kills its self.
-//Then go back to index
+//Then go back to index.
 session_start();
 $_SESSION = null;
 session_destroy();
